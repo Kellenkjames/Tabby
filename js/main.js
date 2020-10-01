dcae55a27d13ed7a6eff3d6ab54f11a29735886c
Kellen <kellenkjames@gmail.com>
2020-09-30 20:04:11 -0700

    rebuilt component for additional practice
    rebuilt component for additional practice

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-/* Switching Tabs 
+const tabby = document.querySelector('.tabby') 
+const tab = Array.from(tabby.querySelectorAll('.tab'))
+const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
 
-1. Add event listeners to each tab
-2. Find the tab they clicked
-3. Find the corresponding tab content
-4. Remove is-selected from other tabs to de-emphasize them
-5. Remove is-selected from other tab content to hide them
-6. Add is-selected to the tab they clicked to emphasize it
-7. Add is-selected to the tab content to show it
-
-*/
-
-const tabs = Array.from(document.querySelectorAll('.tab'))
-const tabby = document.querySelector('.tabby')
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', event => {
-        // Finding the correct tab content 
-        const target = tab.dataset.theme
+tab.forEach(tabBtn => {
+    
+    tabBtn.addEventListener('click', event => {
+        
+        const target = tabBtn.dataset.target
         const tabContent = tabby.querySelector('#' + target)
-        console.log(tabContent)
+        
+        tab.forEach(t => t.classList.remove('is-selected'))
+        tabBtn.classList.add('is-selected')
+
+        tabContents.forEach(c => c.classList.remove('is-selected'))
+        tabContent.classList.add('is-selected')
+
     })
 })
+
