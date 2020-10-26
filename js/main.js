abbd107f9fea723f3730bbe80c7e814504ee729b
Kellen <kellenkjames@gmail.com>
2020-10-25 20:54:50 -0700

    updated event delegation in main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const tabby = document.querySelector('.tabby')
-const tabList = tabby.querySelector('.tabs')
+const tabsList = tabby.querySelector('.tabs')
 
 const tabs = Array.from(tabby.querySelectorAll('.tab'))
 const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
 
-tabList.addEventListener('click', event => {
+tabsList.addEventListener('click', event => {
     const tab = event.target
     const target = tab.dataset.target
     const tabContent = tabby.querySelector('#' + target)
@@ -16,4 +16,5 @@ tabList.addEventListener('click', event => {
     // Remove 'is-selected' from tabContent
     tabContents.forEach(c => { c.classList.remove('is-selected') })
     tabContent.classList.add('is-selected')
+
 })
